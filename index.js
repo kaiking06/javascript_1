abe278faf745732908cb180aeb9a3c58cf0067df
kaiking06 <128317144+kaiking06@users.noreply.github.com>
2023-06-03 22:38:01 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//Array
+
+//const students=['luther','jerry','mende','rosemary']
+
+//students[0]='king'
+//console.log(students)
+
+const towns= ['kumba','limbe','tiko','buea']
+//console.log(towns)
+
+towns[2]='douala'
+console.log(towns)
+
+towns[4]= 'matoh';
+console.log(towns)
